85a8f5817edc9620f7c8afe59cdf42730ab8bb27
keshav-kashyap <kashyapkeshav934@gmail.com>
2025-06-17 22:27:15 +0530

    Your commit message

--- server/server.js
+++ server/server.js
@@ -4,35 +4,26 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const path = require('path');
 
-
 // Import routes
 const userRoutes = require('./routes/userRoutes');
 const studentRoutes = require('./routes/studentRoutes');
 const profileRoutes = require('./routes/profileRoutes');
 
-
-// Load environment variables
 dotenv.config();
-
 const app = express();
 
-// Middleware
+// CORS setup
 const allowedOrigins = [
   'https://surajprintingpressnew.netlify.app',
   'http://localhost:5173'
 ];
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true,
-}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-
-app.use(express.static(path.join(__dirname, '../client/dist')));
-// Database connection
+// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected successfully'))
   .catch(err => console.log('MongoDB connection error:', err));
@@ -42,12 +33,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/students', studentRoutes);
 app.use('/api/profile', profileRoutes);
 
-// Basic route
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-});
-
-// Error handling middleware
+// Error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Something went wrong!' });
