01642f4245a7f4a7ab1962913a2f46fa80d782a1
keshav-kashyap <kashyapkeshav934@gmail.com>
2025-06-19 15:27:09 +0530

    your message

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,4 +1,2 @@
 // src/config/api.js
-export const API_BASE = import.meta.env.DEV 
-  ? "http://localhost:5000" 
-  : "https://student-manager-qpdt.onrender.com";
+export const API_BASE =  "https://student-manager-backend-zbjw.onrender.com";
